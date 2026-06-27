e66d7fd8d2f89e83d9391bdc627e5b22a2466f4b
Dakota Nagy <dnagy6@student.cscc.edu>
2026-06-27 13:58:11 -0400

    Build main game loop with scores, and winner/loser status

--- Lab9_dnagy-1.py
+++ Lab9_dnagy-1.py
@@ -15,8 +15,44 @@ def main():
     player2 = Player("Player 2")
 
     print(f"{player1.get_name()} has {player1.get_wallet()} coins.")
-    print(f"{player2.get_name()} has {player2.get_wallet()} coins.")
+    print(f"{player2.get_name()} has {player2.get_wallet()} coins.\n")
 
     play_again = input("Do you want to toss the coins? (y/n): ").lower()
 
-    
+    while play_again == 'y':
+        player1.toss_coin()
+        player2.toss_coin()
+
+        side1 = player1.get_coin_side()
+        side2 = player2.get_coin_side()
+
+        print(f"{player1.get_name()} tossed: {side1}")
+        print(f"{player2.get_name()} tossed: {side2}")
+
+        if side1 == side2: #Match. Player 1 wins a coin.
+            player1.win_coin()
+            player2.lose_coin()
+            print(f"... {player1.get_name()} wins a coin.")
+        else: #No match. Player 2 wins a coin.
+            player2.win_coin()
+            player1.lose_coin()
+            print(f"... {player2.get_name()} wins a coin.")
+        
+        print(f"\n{player1.get_name()} has {player1.get_wallet()} coins.")
+        print(f"{player2.get_name()} has {player2.get_wallet()} coins.\n")
+
+        play_again = input("Do you want to toss the coins again? (y/n): ").lower()
+
+    print("\n--- Final Score ---")
+    print(f"{player1.get_name()}: {player1.get_wallet()} coins.")
+    print(f"{player2.get_name()}: {player2.get_wallet()} coins.")
+
+    if player1.get_wallet() > player2.get_wallet():
+        print(f"{player1.get_name()} wins the game!")
+    elif player2.get_wallet() > player1.get_wallet():
+        print(f"{player2.get_name()} wins the game!")
+    else:
+        print("It's a draw !")
+
+if __name__ == "__main__":
+    main()
